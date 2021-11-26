f58df542b2bb9fb48896a46fe93f817b9bc3da63
Leonardo Pinheiro <leoprocha100@gmail.com>
2021-11-26 12:38:14 -0300

    feat(plug-checkout): update Plug Checkout version and implement new component API

--- main.js
+++ main.js
@@ -1,13 +1,49 @@
 (() => {
   const plugCheckout = document.querySelector("plug-checkout");
 
-  plugCheckout.customFormStyleClasses = {
-    submitButton: "custom-submit-button",
+  plugCheckout.paymentMethods = {
+    pix: {
+      expiresIn: 3600,
+    },
+    credit: {
+      installments: {
+        quantity: 1,
+        show: true,
+      },
+      showCreditCard: true,
+    },
+    boleto: {
+      expiresDate: "2022-12-31",
+      instructions: "Instruções para pagamento do boleto",
+      interest: {
+        days: 1,
+        amount: 100,
+      },
+      fine: {
+        days: 2,
+        amount: 200,
+      },
+    },
   };
 
-  plugCheckout.installmentsConfig = {
+  plugCheckout.transactionConfig = {
+    statementDescriptor: "#1 Demonstration Plug Checkout",
+    amount: 100,
+    description: "",
+    orderId: "",
+    customerId: "<CUSTOMER_ID>",
+    currency: "BRL",
+    capture: false,
+  };
+
+  plugCheckout.dialogConfig = {
     show: true,
-    quantity: 2,
+    actionButtonLabel: "Continuar",
+    errorActionButtonLabel: "Tentar novamente",
+    successActionButtonLabel: "Continuar",
+    successRedirectUrl: "https://www.plugpagamentos.com/",
+    pixFilledProgressBarColor: "#344383",
+    pixEmptyProgressBarColor: "#D8DFF0",
   };
 
   plugCheckout.addEventListener("paymentSuccess", ({ detail: { data } }) => {
